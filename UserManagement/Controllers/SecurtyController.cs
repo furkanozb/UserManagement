3a16c7394d6aae8682322b9b7654141497e5f686
Furkan Özbay <furkanozb37@hotmail.com>
2020-04-23 06:24:19 +0300

    finish

---
+++ UserManagement/Controllers/SecurtyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    [AllowAnonymous]
+    public class SecurtyController : Controller
+    {
+        UserManagementDbEntities db = new UserManagementDbEntities();
+        // GET: Securty
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            var userInDb = db.Users.FirstOrDefault(x => x.UserName == user.UserName && x.Password == user.Password);
+            if (userInDb != null)
+            {
+                if (userInDb.RoleId == 1)
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("AdminPage", "Home");
+
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("StandartUserPage", "Home");
+
+                }
+            }
+            else
+            {
+                ViewBag.message = "Geçersiz Kullanıcı yada şifre";
+                return View();
+            }
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    [AllowAnonymous]
+    public class UserController : Controller
+    {
+        UserManagementDbEntities db = new UserManagementDbEntities();
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Users user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Login", "Securty");
+        }
+    }
+}
